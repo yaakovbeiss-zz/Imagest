55165520cb9c91e138de8204b94d8925139d2236
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-07-03 13:15:28 -0400

    Fix Search Bar submit

--- frontend/components/header/search_bar.jsx
+++ frontend/components/header/search_bar.jsx
@@ -8,9 +8,10 @@ class SearchBar extends React.Component {
     super(props);
     this.state = {
       searchResults: '',
-      hiddenSearchBar: true,
+      hideSearchBar: true,
     }
      this.handleChange = this.handleChange.bind(this);
+     this.toggleSearchBar = this.toggleSearchBar.bind(this);
   }
 
   handleChange() {
@@ -30,22 +31,34 @@ class SearchBar extends React.Component {
     }
   }
 
+  toggleSearchBar(){
+    this.setState({ hideSearchBar: !this.state.hideSearchBar })
+  }
+
+  searchBar(){
+    return this.state.hideSearchBar ? <div className="hidden-div"></div> :
+    <div id="search-icon-menu" className="">
+      <form onSubmit={this.toggleSearchBar}>
+        <input type="text" onChange={this.handleChange('searchResults')} className="search-bar"></input>
+        <div className="search-bar search-bar-addition">SEARCH SYNTAX</div>
+      </form>
+    </div>
+  }
+
   render(){
     return (
         <form>
           <div className="right-nav">
-          <div id="search-icon-menu" className="hidden">
-            <input type="text" onChange={this.handleChange('searchResults')} className="search-bar"></input>
-            <div className="search-bar search-bar-addition">SEARCH SYNTAX</div>
-          </div>
+            {this.searchBar()}
+
           <div className="search-icon-container" >
             <div className="icon-container">
-              <img className="search-icon" id="search-icon" onClick={this.toggleDrop} src={window.images.search_icon}/>
+              <img className="search-icon" id="search-icon" onClick={this.toggleSearchBar} src={window.images.search_icon}/>
             </div>
               <GreetingContainer />
           </div>
         </div>
-          <input type="hidden" onSubmit={this.toggleDrop} className="hidden-input"  />
+
         </form>
     )
   }
